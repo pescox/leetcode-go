a993f2bb58ceb9acb73437e544322d1dbc7873e1
Pesco Ding <dp@macintosh.local>
2022-05-22 11:43:18 +0800

    update 146

--- 146_lru-cache/main.go
+++ 146_lru-cache/main.go
@@ -1,51 +1,130 @@
 package main
 
 import (
-	"container/list"
+	"log"
 )
 
-func main() {}
+func main() {
+	cache := Constructor(2)
+	cache.Put(1, 1)
+	log.Printf("cache1: %#+v\n", cache)
+	cache.Put(2, 2)
+	log.Printf("cache2: %#+v\n", cache)
+	cache.Get(1)
+	log.Printf("cache3: %#+v\n", cache)
+	cache.Put(3, 3)
+	log.Printf("cache4: %#+v\n", cache)
+}
 
-type item struct {
+type node struct {
 	key   int
 	value int
+	prev  *node
+	next  *node
+}
+
+func newNode(key, value int) *node {
+	return &node{
+		key:   key,
+		value: value,
+	}
+}
+
+type doubleList struct {
+	head *node
+	tail *node
+	len  int
+}
+
+func newDoubleList() *doubleList {
+	return &doubleList{}
+}
+
+func (l *doubleList) add(n *node) {
+	if l.tail == nil {
+		l.head = n
+		l.tail = n
+		l.len++
+		return
+	}
+	l.tail.next = n
+	n.prev = l.tail
+	l.tail = n
+	l.len++
+}
+
+func (l *doubleList) deleteFirst() {
+	if l.head == nil {
+		return
+	}
+	if l.head.next == nil {
+		l.head = nil
+		l.tail = nil
+		l.len--
+		return
+	}
+
+	l.head = l.head.next
+	l.head.prev = nil
+	l.len--
+}
+
+func (l *doubleList) delete(n *node) {
+	if n.prev == nil {
+		l.deleteFirst()
+		return
+	}
+	if n.next == nil {
+		l.tail = n.prev
+	} else {
+		n.next.prev = n.prev
+	}
+
+	n.prev.next = n.next
+	n.prev = nil
+	n.next = nil
+	l.len--
 }
 
 type LRUCache struct {
 	capacity int
-	list     *list.List
-	m        map[int]*list.Element
+	m        map[int]*node
+	l        *doubleList
 }
 
 func Constructor(capacity int) LRUCache {
 	return LRUCache{
 		capacity: capacity,
-		list:     list.New(),
-		m:        make(map[int]*list.Element),
+		m:        make(map[int]*node, capacity),
+		l:        newDoubleList(),
 	}
 }
 
 func (this *LRUCache) Get(key int) int {
-	if ele, ok := this.m[key]; ok {
-		this.list.MoveToFront(ele)
-		return ele.Value.(item).value
+	n, ok := this.m[key]
+	if !ok {
+		return -1
 	}
-	return -1
+
+	this.l.delete(n)
+	this.l.add(n)
+	return n.value
 }
 
 func (this *LRUCache) Put(key int, value int) {
-	if ele, ok := this.m[key]; ok {
-		this.list.MoveToFront(ele)
+	new := newNode(key, value)
+	if old, ok := this.m[key]; ok {
+		this.l.delete(old)
+		this.l.add(new)
+		this.m[key] = new
 		return
 	}
-
-	if len(this.m) == this.capacity {
-		delete(this.m, this.list.Back().Value.(item).key)
-		this.list.Remove(this.list.Back())
+	if this.capacity == len(this.m) {
+		delete(this.m, this.l.head.key)
+		this.l.deleteFirst()
 	}
-
-	ele := this.list.PushFront(item{key: key, value: value})
-	this.m[key] = ele
+	this.m[key] = new
+	this.l.add(new)
 }
 
 /**
